489257fdef30dcd62e128d5150dcd2342360eed1
Arka Mitra <arka2k8@gmail.com>
2019-10-14 19:52:04 +0100

    Initial commit with basic SpringBoot code

---
+++ src/main/java/uk/co/rightmove/propertyanalyser/PropertyAnalyserApplication.java
@@ -0,0 +1,14 @@
+package uk.co.rightmove.propertyanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyAnalyserApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PropertyAnalyserApplication.class, args);
+	}
+
+}
